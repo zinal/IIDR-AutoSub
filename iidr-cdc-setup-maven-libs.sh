68f157e1cc71416a14059e5e85622b8947a82642
Maksim Zinal <mzinal@ru.ibm.com>
2020-11-29 18:28:47 +0300

    set the dependency versions as a variable

--- iidr-cdc-setup-maven-libs.sh
+++ iidr-cdc-setup-maven-libs.sh
@@ -7,6 +7,15 @@ MVN="/Applications/NetBeans/NetBeans 8.2.app/Contents/Resources/NetBeans/java/ma
 
 XVER=11.4.0.2.10686
 
+# api
+# chcclp
+# comms
+# messaging
+# online
+# resources
+# server
+
+
 ls *.jar | while read fn; do
 
 ARTI=`basename "$fn" .jar`
