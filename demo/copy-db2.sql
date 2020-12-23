5fa4d95c531516de8b176462e34fbdf989f96af1
Maksim Zinal <mzinal@ru.ibm.com>
2020-12-23 15:54:29 +0300

    updated schema and procedures for Db2 demo

--- demo/copy-db2.sql
+++ demo/copy-db2.sql
@@ -11,3 +11,54 @@ CREATE TABLE cdcbak.tab6 AS (SELECT * FROM cdcdemo.tab6) WITH DATA IN cdcdemo;
 CREATE TABLE cdcbak.tab7 AS (SELECT * FROM cdcdemo.tab7) WITH DATA IN cdcdemo;
 CREATE TABLE cdcbak.tab8 AS (SELECT * FROM cdcdemo.tab8) WITH DATA IN cdcdemo;
 CREATE TABLE cdcbak.tab9 AS (SELECT * FROM cdcdemo.tab9) WITH DATA IN cdcdemo;
+
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab0);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab0;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab1);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab1;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab2);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab2;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab3);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab3;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab4);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab4;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab5);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab5;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab6);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab6;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab7);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab7;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab8);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab8;
+
+DECLARE c1 CURSOR FOR (SELECT a,b,'*' AS l, c FROM cdcbak.tab9);
+LOAD FROM c1 OF CURSOR REPLACE INTO cdcdemo.tab9;
+
+(SELECT 0 AS num, COUNT(*) AS cnt FROM cdcdemo.tab0)
+UNION ALL
+(SELECT 1 AS num, COUNT(*) AS cnt FROM cdcdemo.tab1)
+UNION ALL
+(SELECT 2 AS num, COUNT(*) AS cnt FROM cdcdemo.tab2)
+UNION ALL
+(SELECT 3 AS num, COUNT(*) AS cnt FROM cdcdemo.tab3)
+UNION ALL
+(SELECT 4 AS num, COUNT(*) AS cnt FROM cdcdemo.tab4)
+UNION ALL
+(SELECT 5 AS num, COUNT(*) AS cnt FROM cdcdemo.tab5)
+UNION ALL
+(SELECT 6 AS num, COUNT(*) AS cnt FROM cdcdemo.tab6)
+UNION ALL
+(SELECT 7 AS num, COUNT(*) AS cnt FROM cdcdemo.tab7)
+UNION ALL
+(SELECT 8 AS num, COUNT(*) AS cnt FROM cdcdemo.tab8)
+UNION ALL
+(SELECT 9 AS num, COUNT(*) AS cnt FROM cdcdemo.tab9);
--- demo/database-db2.sql
+++ demo/database-db2.sql
@@ -19,60 +19,70 @@ DROP TABLE cdcdemo.tab9;
 CREATE TABLE cdcdemo.tab0 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab1 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab2 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab3 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab4 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab5 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab6 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab7 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab8 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
 CREATE TABLE cdcdemo.tab9 (
   A INTEGER NOT NULL PRIMARY KEY,
   B VARCHAR(100) NOT NULL,
+  L VARCHAR(10) NOT NULL,
   C TIMESTAMP NOT NULL
 ) IN cdcdemo @
 
@@ -85,6 +95,7 @@ BEGIN
     SET v_sql = 'INSERT INTO cdcdemo.tab' || v_i;
     SET v_sql = v_sql || ' SELECT row_number() OVER ()';
     SET v_sql = v_sql || ' ,dbms_random.string(''x'', dbms_random.value(10, 50))';
+    SET v_sql = v_sql || ' , ''*''';
     SET v_sql = v_sql || ' ,CURRENT TIMESTAMP';
     SET v_sql = v_sql || ' FROM (SELECT ''a'' AS x FROM syscat.columns FETCH FIRST 1000 ROWS ONLY) r1,';
     SET v_sql = v_sql || ' (SELECT ''a'' AS y FROM syscat.columns FETCH FIRST 1000 ROWS ONLY) r2';
@@ -98,80 +109,74 @@ BEGIN
 END
 @
 
-CALL cdcdemo.datagen @
-
-
-CREATE OR REPLACE PROCEDURE cdcdemo.action1
-LANGUAGE  SQL
+CREATE OR REPLACE PROCEDURE cdcdemo.action1 (
+  IN p_label VARCHAR(10),
+  IN p_id INTEGER
+) LANGUAGE  SQL
 BEGIN
   DECLARE v_b VARCHAR(100);
-  DECLARE v_id INTEGER;
-  SELECT DBMS_RANDOM.VALUE(1, 1000000),
-         '1+' || dbms_random.string('x', dbms_random.value(10, 30))
-    INTO v_id, v_b FROM sysibm.sysdummy1;
-  UPDATE cdcdemo.tab0 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab3 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab6 SET b=v_b, c=SYSDATE WHERE a=v_id;
+  SELECT 'A1+' || dbms_random.string('x', dbms_random.value(10, 30))
+    INTO v_b FROM sysibm.sysdummy1;
+  UPDATE cdcdemo.tab0 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
+  UPDATE cdcdemo.tab5 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
   COMMIT;
 END
 @
 
-CREATE OR REPLACE PROCEDURE cdcdemo.action2
-LANGUAGE  SQL
+CREATE OR REPLACE PROCEDURE cdcdemo.action2 (
+  IN p_label VARCHAR(10),
+  IN p_id INTEGER
+) LANGUAGE  SQL
 BEGIN
   DECLARE v_b VARCHAR(100);
-  DECLARE v_id INTEGER;
-  SELECT DBMS_RANDOM.VALUE(1, 1000000),
-         '2+' || dbms_random.string('x', dbms_random.value(10, 30))
-    INTO v_id, v_b FROM sysibm.sysdummy1;
-  UPDATE cdcdemo.tab1 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab4 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab7 SET b=v_b, c=SYSDATE WHERE a=v_id;
+  SELECT 'A2+' || dbms_random.string('x', dbms_random.value(10, 30))
+    INTO v_b FROM sysibm.sysdummy1;
+  UPDATE cdcdemo.tab1 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
+  UPDATE cdcdemo.tab6 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
   COMMIT;
 END
 @
 
-CREATE OR REPLACE PROCEDURE cdcdemo.action3
-LANGUAGE  SQL
+CREATE OR REPLACE PROCEDURE cdcdemo.action3 (
+  IN p_label VARCHAR(10),
+  IN p_id INTEGER
+) LANGUAGE  SQL
 BEGIN
   DECLARE v_b VARCHAR(100);
-  DECLARE v_id INTEGER;
-  SELECT DBMS_RANDOM.VALUE(1, 1000000),
-         '3+' || dbms_random.string('x', dbms_random.value(10, 30))
-    INTO v_id, v_b FROM sysibm.sysdummy1;
-  UPDATE cdcdemo.tab2 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab5 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab8 SET b=v_b, c=SYSDATE WHERE a=v_id;
+  SELECT 'A3+' || dbms_random.string('x', dbms_random.value(10, 30))
+    INTO v_b FROM sysibm.sysdummy1;
+  UPDATE cdcdemo.tab2 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
+  UPDATE cdcdemo.tab7 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
   COMMIT;
 END
 @
 
-CREATE OR REPLACE PROCEDURE cdcdemo.action4
-LANGUAGE  SQL
+CREATE OR REPLACE PROCEDURE cdcdemo.action4 (
+  IN p_label VARCHAR(10),
+  IN p_id INTEGER
+) LANGUAGE  SQL
 BEGIN
   DECLARE v_b VARCHAR(100);
-  DECLARE v_id INTEGER;
-  SELECT DBMS_RANDOM.VALUE(1, 1000000),
-         '4+' || dbms_random.string('x', dbms_random.value(10, 30))
-    INTO v_id, v_b FROM sysibm.sysdummy1;
-  UPDATE cdcdemo.tab3 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab6 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab9 SET b=v_b, c=SYSDATE WHERE a=v_id;
+  SELECT 'A4+' || dbms_random.string('x', dbms_random.value(10, 30))
+    INTO v_b FROM sysibm.sysdummy1;
+  UPDATE cdcdemo.tab3 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
+  UPDATE cdcdemo.tab8 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
   COMMIT;
 END
 @
 
-CREATE OR REPLACE PROCEDURE cdcdemo.action5
-LANGUAGE  SQL
+CREATE OR REPLACE PROCEDURE cdcdemo.action5 (
+  IN p_label VARCHAR(10),
+  IN p_id INTEGER
+) LANGUAGE  SQL
 BEGIN
   DECLARE v_b VARCHAR(100);
-  DECLARE v_id INTEGER;
-  SELECT DBMS_RANDOM.VALUE(1, 1000000),
-         '5+' || dbms_random.string('x', dbms_random.value(10, 30))
-    INTO v_id, v_b FROM sysibm.sysdummy1;
-  UPDATE cdcdemo.tab0 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab4 SET b=v_b, c=SYSDATE WHERE a=v_id;
-  UPDATE cdcdemo.tab7 SET b=v_b, c=SYSDATE WHERE a=v_id;
+  SELECT 'A5+' || dbms_random.string('x', dbms_random.value(10, 30))
+    INTO v_b FROM sysibm.sysdummy1;
+  UPDATE cdcdemo.tab4 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
+  UPDATE cdcdemo.tab9 SET b=v_b, l=p_label, c=CURRENT TIMESTAMP WHERE a=p_id;
   COMMIT;
 END
 @
+
+-- CALL cdcdemo.datagen @
