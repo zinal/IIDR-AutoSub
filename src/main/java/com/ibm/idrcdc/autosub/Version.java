bf696233c54f1f3083675bd7529fb499dd8414c0
Maksim Zinal <mzinal@ru.ibm.com>
2021-12-19 15:12:15 +0300

    bump log4j to 2.17.0 for once more vulnurability

--- src/main/java/com/ibm/idrcdc/autosub/Version.java
+++ src/main/java/com/ibm/idrcdc/autosub/Version.java
@@ -29,6 +29,6 @@ public class Version {
 
     // development format: x.y-SNAPSHOT
     // release format: x.y-YYYY.MM.DD
-    public static final String VERSION = "2.4-SNAPSHOT";
+    public static final String VERSION = "2.4-2021.12.19";
 
 }
